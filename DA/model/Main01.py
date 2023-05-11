13054cd4ea127623409d3ed53fd4f270c42c2ea7
내 이름 < >
2023-05-11 17:28:11 +0900

    05_11_05tl 수정

--- DA/model/Main01.py
+++ DA/model/Main01.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-excel_file = 'D:/장우영/LOCALSEARCH/ShipMovementPrediction_Project/teamproject-main/DA/data/해양기상_통계자료_신항유도등.xls'
+excel_file = 'D:/장우영/LOCALSEARCH\DA\DA\data/05_09해양 기상청 데이터.xls'
 
 df = pd.read_excel(excel_file)
 #   print(df.shape)
@@ -15,6 +15,9 @@ print(df)
 df.dropna(inplace=True)
 #print(df.head(5))
 
-#     지방청            표지                    일시            풍향(˚)  유향(˚)  기온(℃)  풍속(m/s)  유속(kn)  기압(hPa)  습도(%)
-#    부산청  부산항유도등부표(랜비)         2023-05-08 23:50      0        189     14.4       3.08      0.9      1015.0     57
- 
+#  지방청            표지                일시023-05-09 17:40	2	     168	    15.8	 4.11	    0.4		             73
+#  부산청	부산항유도등부표(랜비)	2023-05-09 17:30	5	     178	    16	     4.11	    0.2		             68
+#  부산청	부산항유도등부표(랜비)	2023-05-09 17:20	7	     196        15.8	 3.08	    0.3		             68
+#  부산청	부산항유도등부표(랜비)	2023-05-09 17:10	266	     105	    16.4	 2.57	    0.1		             74
+#  부산청	부산항유도등부표(랜비)	2023-05-09 17:00         풍향(˚)  유향(˚)  기온(℃)  풍속(m/s)  유속(kn)   기압(hPa)   습도(%)
+#  부산청	부산항유도등부표(랜비)	2	219	     210	    16.5	 4.63	    0.1		             74
--- DA/model/Main02.py
+++ DA/model/Main02.py
@@ -2,7 +2,7 @@ import pandas as pd
 import csv
 
 # txt 파일을 읽어와서 파싱하여 csv 형식으로 저장
-with open('DA/data/time.txt', 'r') as infile, open('time.csv', 'w', newline='') as outfile:
+with open('DA/data/log.txt', 'r') as infile, open('log.csv', 'w', newline='') as outfile:
     writer = csv.writer(outfile)
     for line in infile:
         # txt 파일의 구분자를 기준으로 데이터를 분리하여 리스트로 만듦
@@ -12,11 +12,11 @@ with open('DA/data/time.txt', 'r') as infile, open('time.csv', 'w', newline='') 
 
 
 
-a = pd.read_csv('time.csv', thousands=',',encoding='euc-kr')
-#print(a)
+a = pd.read_csv('log.csv', thousands=',',encoding='euc-kr')
+print(a)
 
 
-with open('time.csv', 'r') as f:
+with open('log.csv', 'r') as f:
     reader = csv.reader(f)
     for row in reader:
         fields = row[0].split(' ')
@@ -24,16 +24,8 @@ with open('time.csv', 'r') as f:
         mmsi = fields[4]
         posX = fields[8]
         posY = fields[10]
-        print(f'time: {time}, mmsi: {mmsi}, posX: {posX}, posY: {posY}')
-        
-from datetime import datetime
+        print(f'time: {time}, mmsi: {mmsi},posX: {posX}, posY: {posY}')
 
-date_str = '5/9/2023'
-date_obj = datetime.strptime(date_str, '%m/%d/%Y')
-formatted_date = date_obj.strftime('%Y-%m-%d')
 
-print(formatted_date) 
-        
-        
-        
-# time: 5/9/2023, 17:50:46, mmsi: 636018242, posX: MSC, posY: VI
+#   time:5/9/2023, 17:11:10 mmsi:413219020 shipType:70 shipName:ZHONG WAI YUN NINGBO posX:129.30875714057603 posY:34.97518208647576
+
