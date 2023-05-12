7860fc04184d8e95dd0ba452ab5219cfbfde5f39
내 이름 < >
2023-05-12 15:04:27 +0900

    05_12_전처리
    데이터 파일 전처리 수행

--- DA/model/Main02.py
+++ DA/model/Main02.py
@@ -1,31 +1,21 @@
 import pandas as pd
 import csv
 
-# txt 파일을 읽어와서 파싱하여 csv 형식으로 저장
-with open('DA/data/log.txt', 'r') as infile, open('log.csv', 'w', newline='') as outfile:
-    writer = csv.writer(outfile)
-    for line in infile:
-        # txt 파일의 구분자를 기준으로 데이터를 분리하여 리스트로 만듦
-        data = line.strip().split('\t')
-        # csv 파일에 데이터를 저장
-        writer.writerow(data)
+df = pd.read_csv('D:/장우영/LOCALSEARCH/DA/log.csv')
+#print(df)
+print(df.head(1))
+
+
+#with open('log.csv', 'r') as f:
+#    reader = csv.reader(f)
+#    for row in reader:
+#        fields = row[0].split(' ')
+#        time = fields[0] + fields[1] + ','+  fields[2]
+#        posX = fields[5]
+#        posY = fields[6]
+#        print(f'{time},posX: {posX}, posY: {posY}')
 
 
 
-a = pd.read_csv('log.csv', thousands=',',encoding='euc-kr')
-print(a)
-
-
-with open('log.csv', 'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        fields = row[0].split(' ')
-        time = fields[1] + ' ' + fields[2]
-        mmsi = fields[4]
-        posX = fields[8]
-        posY = fields[10]
-        print(f'time: {time}, mmsi: {mmsi},posX: {posX}, posY: {posY}')
-
-
-#   time:5/9/2023, 17:11:10 mmsi:413219020 shipType:70 shipName:ZHONG WAI YUN NINGBO posX:129.30875714057603 posY:34.97518208647576
+#time:5/9/2023, 17:11:10 mmsi:413219020 shipType:70 shipName:ZHONG WAI YUN NINGBO posX:129.30875714057603 posY:34.97518208647576
 
--- DA/model/Model.py
+++ DA/model/Model.py
@@ -1,26 +1,15 @@
 # 입력으로는 시간에 따른 선박의 위치 정보와 해당 시간의 기상 데이터를 사용하고, 
 # 출력으로는 다음 시간대의 선박 위치 정보를 예측합니다.
 
-import numpy as np
-import pandas as np
-from keras.models import Sequential
-from keras.layers import LSTM, Dense, Dropout
+import pandas as pd
+import csv
 
-# AIS 데이터와 기상 데이터를 불러와 입력 데이터와 출력 데이터를 구성합니다.
-# 입력 데이터는 (시간, AIS 데이터 차원 + 기상 데이터 차원)의 2D 배열 형태를 갖습니다.
-# 출력 데이터는 (시간, AIS 데이터 차원)의 2D 배열 형태를 갖습니다.
-X_train, y_train, X_val, y_val = load_data()
 
-# LSTM 모델을 생성합니다.
-model = Sequential()
-model.add(LSTM(64, input_shape=(X_train.shape[1], X_train.shape[2]), return_sequences=True))
-model.add(Dropout(0.2))
-model.add(LSTM(32))
-model.add(Dropout(0.2))
-model.add(Dense(y_train.shape[1]))
+excel02_file = 'D:/장우영/LOCALSEARCH/DA/log.csv'
 
-# 모델을 컴파일합니다.
-model.compile(loss='mean_squared_error', optimizer='adam')
+df = pd.read_excel(excel02_file)
+#   print(df.shape)
 
-# 모델을 학습시킵니다.
-model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=50, batch_size=64)
+# 데이터 확인 
+
+print(df.head(1))
