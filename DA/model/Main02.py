835b80a27913322bd978b57f9c2e1677e76f9ad4
내 이름 <122770858+JWY0717@users.noreply.github.com>
2023-05-17 16:09:11 +0900

    05.17수정

--- DA/model/Main02.py
+++ DA/model/Main02.py
@@ -1,21 +1,8 @@
 import pandas as pd
 import csv
 
-df = pd.read_csv('D:/장우영/LOCALSEARCH/DA/log.csv')
-#print(df)
-print(df.head(1))
+df = 'D:/장우영/LOCALSEARCH\DA\DA\data/AIS.xlsx'
 
-
-#with open('log.csv', 'r') as f:
-#    reader = csv.reader(f)
-#    for row in reader:
-#        fields = row[0].split(' ')
-#        time = fields[0] + fields[1] + ','+  fields[2]
-#        posX = fields[5]
-#        posY = fields[6]
-#        print(f'{time},posX: {posX}, posY: {posY}')
-
-
-
-#time:5/9/2023, 17:11:10 mmsi:413219020 shipType:70 shipName:ZHONG WAI YUN NINGBO posX:129.30875714057603 posY:34.97518208647576
+ais_data = pd.read_excel(df)
+print(ais_data)
 
