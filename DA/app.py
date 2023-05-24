1b4d1697543360298f491302b36c0c01146cede6
내 이름 <122770858+JWY0717@users.noreply.github.com>
2023-05-24 18:00:58 +0900

    05.24 모델 수정

--- DA/app.py
+++ DA/app.py
@@ -1,40 +1,62 @@
-from flask import Flask, request, jsonify
+import pandas as pd
 import numpy as np
+import pickle
+from pyproj import Transformer
+from flask import Flask, request, jsonify
 
-# Create Flask app
 app = Flask(__name__)
 
-# API endpoint for prediction
-DETECTION_URL = '/api/predict'
+# Load the trained model and scaler
+model = pickle.load(open('DA/notebooks/linear_regression_model.pkl', 'rb'))
 
-# Define route for prediction API
-@app.route(DETECTION_URL, methods=['POST'])
+#Load the coordinate transformer
+transformer = Transformer.from_crs(3857, 4326, always_xy=True)
 
+def convert_coordinates(coord):
+        # 빈 문자열인 경우 처리
+    if not coord:
+        return None 
+
+    try:
+        # 좌표값을 파싱하여 x, y 좌표 추출
+        x = int(coord[8:24], 16) / 10000000.0
+        y = int(coord[24:40], 16) / 10000000.0
+
+        # 좌표 변환
+        lon, lat = transformer.transform(x, y)
+
+        return lon, lat
+    except ValueError:
+        # 유효하지 않은 형식의 좌표값인 경우 처리
+        return None
+
+@app.route('/predict', methods=['POST'])
 def predict():
-    # Get the input data from the request
-    input_data = request.json
+    # Get the request data
+    data = request.get_json()
+
+    # Preprocess the input data
+    input_data = pd.DataFrame(data["data"])
+    input_data['geom'] = input_data['geom'].apply(convert_coordinates)
+    input_data['경도'] = input_data['geom'].apply(lambda x: x[0] if x is not None else None)
+    input_data['위도'] = input_data['geom'].apply(lambda x: x[1] if x is not None else None)
+    input_data['insert_time'] = pd.to_datetime(input_data['insert_time']).astype('int64') // 10**9
+
+    # Select the features for prediction (excluding 'geom') !!!
+    features = ["mmsi", "ship_type", "경도", "위도", "cog", "sog", "insert_time", "풍향", "유향", "기온", "수온", "풍속", "유속", "기압", "습도"]
+    input_data = input_data[features]
     
-    # Perform any necessary preprocessing on the input data
-    # ...
-    
-    # Convert input data to numpy array
-    input_array = np.array([input_data], dtype=np.float32)
-    
-    # Make predictions using the model
-    predictions = model.predict(input_array)
-    
-    # Perform any necessary post-processing on the predictions
-    # ...
-    
-    # Create a response dictionary with the predictions
-    response = {
-        'predictions': predictions.tolist()
-    }
-    
-    # Return the response as JSON
+    # Make predictions
+    predictions = model.predict(input_data)
+
+    # Prepare the response
+    response = {"predictions": predictions.tolist()}
+
     return jsonify(response)
 
-# Run the Flask app
 if __name__ == '__main__':
-    app.run()
+    # Run the Flask app
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
+
 
